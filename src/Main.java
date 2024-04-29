287d1b341bc41de73b025397e7b56b9e222384ff
Николай <kolya-91@list.ru>
2024-04-29 13:28:04 +0500

    Вариант без массива

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         ServiceStantion serviceStantion = new ServiceStantion();
+        Car car = new Car("car", 4);
+        serviceStantion.check(car);
 
-        Transport[] transports = new Transport[7];
-        transports[0] = new Bicycle("bicycle1", 2);
-        transports[1] = new Bicycle("bicycle2", 2);
-        transports[3] = new Car("car1", 4);
-        transports[4] = new Car("car2", 4);
-        transports[5] = new Truck("truck1", 4);
-        transports[6] = new Truck("truck2", 6);
-        for (Transport transport : transports) {
-            serviceStantion.check(transport);
+        Bicycle bicycle = new Bicycle("велик", 2);
+        serviceStantion.check(bicycle);
+
+        Truck truck = new Truck("truck", 8);
+        serviceStantion.check(truck);
         }
     }
-}
