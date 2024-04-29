01bcefeba646a219eb1c9b35a68d90e508f5c5e9
Николай <kolya-91@list.ru>
2024-04-29 12:02:21 +0500

    Начало

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    protected String modelName;
+    protected int wheelsCount;
+    protected Car(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
